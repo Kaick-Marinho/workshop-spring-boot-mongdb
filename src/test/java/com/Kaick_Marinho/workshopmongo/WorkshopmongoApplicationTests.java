b97d6b40518e602f16cc319fbdce310feb5bb9d1
Kaick Marinho <kaickmarinho123@gmail.com>
2025-09-18 17:50:31 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/Kaick_Marinho/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Kaick_Marinho.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
